12bf322556955e030d96b828ced4ec1097b73bf1
JHays222 <108699497+JHays222@users.noreply.github.com>
2025-03-05 15:50:56 -0500

    Did a whole lot of things

--- app/src/index.js
+++ app/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+
+import Home from "./pages/home/home"
+import Map from "./pages/map/map"
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path='/map' element={<Map />}/>
+      </Routes>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ app/src/pages/home/home.jsx
@@ -0,0 +1,31 @@
+import './home.css'
+
+function Home(){
+    return(
+    <div className='home'>
+        <div className='container'>
+            <div className='top'>
+                Hike of the Day
+            </div>
+            <div className='bottom'>
+                <div className='item'>
+                    <a><button className='sign-up'>Sign Up</button></a>
+                </div>
+                <div className='item'>
+                    <span>Already have an account?</span>
+                    <a><button className='login'>Login</button></a>
+                </div>
+                <div className='item'>
+                    <a href='/map'><button className='skip'>Skip To map</button></a>
+                </div>
+                
+            </div>
+
+        </div>
+       
+        
+    </div>
+    )
+}
+
+export default Home
