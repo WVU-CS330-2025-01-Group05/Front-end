faa5e09577a0e84772eae4266500839f5ce285fc
JHays222 <julianhays5@gmail.com>
2025-03-06 15:23:45 -0500

    F

--- app/src/pages/map/map.jsx
+++ app/src/pages/map/map.jsx
@@ -1,76 +1,92 @@
 import './map.css'
 
-function Map(){
-    return(
-    <div className='map'>
-        <div className='header'>
-            <span>Time:</span>
-            <span>Plan Your Hike</span>
-            <span>Account</span>
-        </div>
-        <div className='bottom'>
-            <div className='left'>
-                <div className='filter'>
-                    <span>Select Location</span>
-                    <div className='select'>
-                        <div className='selected-option'>
-                            <span>Location</span>
-                        </div>
-                        <div className='options'>
-                            <span>Location</span>
-                            <span>Location</span>
-                            <span>Location</span>
-                            <span>Location</span>
-                            <span>Location</span>
-                        </div>
-                    </div>
-                </div>
-                <div className='filter'>
-                    <span>Select Trail</span>
-                    <div className='select'>
-                        <div className='options'>
-                            <span>Trail</span>
-                            <span>Trail</span>
-                            <span>Trail</span>
-                            <span>Trail</span>
-                            <span>Trail</span>
-                        </div>
-                    </div>
-                </div>
-                <div className='temp'>
 
-                </div>
 
-                <div className='stats'>
-                    <div className='left-stats'>
-                        <div className='item'>
-                            <span>Stat</span>
-                        </div>
-                        <div className='item'>
-                            <span>Stat</span>
-                        </div>
-                        <div className='item'>
-                            <span>Stat</span>
+
+function Map() {
+
+
+
+    return (
+        <div className='map'>
+            <div className='header'>
+                <span>Time:</span>
+                <span>Plan Your Hike</span>
+                <span>Account</span>
+            </div>
+            <div className='bottom'>
+                <div className='left'>
+                    <div className='filter'>
+                        <span>Select Location</span>
+                        <div className='select'>
+                            <div className='selected-option'>
+                                <span>Selected Location</span>
+                            </div>
+                            <div className='options'>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                                <span>Location</span>
+                            </div>
                         </div>
                     </div>
-                    <div className='right-stats'>
-                        <div className='item'>
-                            <span>Stat</span>
-                        </div>
-                        <div className='item'>
-                            <span>Stat</span>
-                        </div>
-                        <div className='item'>
-                            <span>Stat</span>
+                    <div className='filter'>
+                        <span>Select Trail</span>
+                        <div className='select'>
+                            <div className='options'>
+                                <span>Trail</span>
+                                <span>Trail</span>
+                                <span>Trail</span>
+                                <span>Trail</span>
+                                <span>Trail</span>
+                            </div>
                         </div>
                     </div>
+                    <div className='temp'>
+
+                    </div>
+
+                    <div className='stats'>
+                        <div className='left-stats'>
+                            <div className='item'>
+                                <span>Stat</span>
+                            </div>
+                            <div className='item'>
+                                <span>Stat</span>
+                            </div>
+                            <div className='item'>
+                                <span>Stat</span>
+                            </div>
+                        </div>
+                        <div className='right-stats'>
+                            <div className='item'>
+                                <span>Stat</span>
+                            </div>
+                            <div className='item'>
+                                <span>Stat</span>
+                            </div>
+                            <div className='item'>
+                                <span>Stat</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='right'>
+                    <img />
                 </div>
             </div>
-            <div className='right'>
-                <img/>
-            </div>
         </div>
-    </div>
     )
 }
 
