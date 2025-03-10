5e4a02303c5833e87ce14d8a0625473838f2d666
kiersdoes <kd00068@mix.wvu.edu>
2025-03-10 18:49:11 -0400

    Update login.js

--- app/src/pages/login/login.js
+++ app/src/pages/login/login.js
@@ -1,4 +1,4 @@
 <script>
-  document.getElementId("login").addEventListener('click', function () { window.locaton.href = "map.jsx" });
+   document.getElementId("login").addEventListener('click', function() {window.locaton.href = "http://localhost:3000/map" });
 </script>
                                                 
