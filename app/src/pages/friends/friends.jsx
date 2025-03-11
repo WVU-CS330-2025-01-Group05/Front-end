b20b8c44d9436a6e9d6bdacbda4bc2ee839bf2d9
Grace Hanson <hansongrace2004@gmail.com>
2025-03-11 13:41:49 -0400

    Update friends files
    NOT FINISHED- started adding box. need to format it

--- app/src/pages/friends/friends.jsx
+++ app/src/pages/friends/friends.jsx
@@ -1,15 +1,16 @@
 import './friends.css'
 
 function Friends(){
-
     return(
         <div className='friendslist'>
             <div className='top'>
                 Friends List
             </div>
+            <div className='white-box'>
+                <p></p>
             </div>
+        </div>
     )
-    
 }
 
 export default Friends
