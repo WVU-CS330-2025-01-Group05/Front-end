bb89242a6e58d06d65d191f7dd333b8b6b3bc4bc
JHays222 <108699497+JHays222@users.noreply.github.com>
2025-03-12 12:29:24 -0400

    Changes

--- app/src/pages/map/map.jsx
+++ app/src/pages/map/map.jsx
@@ -1,38 +1,35 @@
 import './map.css'
 
+function Map(){
 
 
 
-function Map() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+
+    function dropDownMenu(){
+
+    }
 
 
-
-    return (
-        <div className='map'>
-            <div className='header'>
-                <span>Time:</span>
-                <span>Plan Your Hike</span>
-                <span>Account</span>
-            </div>
-            <div className='bottom'>
-                <div className='left'>
+    return(
+    <div className='map'>
+        <div className='header'>
+            <span>Time: </span>
+            <span>Plan Your Hike</span>
+            <span>Account</span>
+        </div>
+        <div className='bottom'>
+            <div className='left'>
+                <div className='filters'>
                     <div className='filter'>
                         <span>Select Location</span>
                         <div className='select'>
-                            <div className='selected-option'>
-                                <span>Selected Location</span>
-                            </div>
+                            <button id='dropDownButton' className='selected-option' onClick='dropDownMenu'>
+                                <span>Location</span>
+                            </button>
                             <div className='options'>
-                                <span>Location</span>
-                                <span>Location</span>
-                                <span>Location</span>
-                                <span>Location</span>
-                                <span>Location</span>
-                                <span>Location</span>
-                                <span>Location</span>
-                                <span>Location</span>
-                                <span>Location</span>
-                                <span>Location</span>
                                 <span>Location</span>
                                 <span>Location</span>
                                 <span>Location</span>
@@ -53,40 +50,41 @@ function Map() {
                             </div>
                         </div>
                     </div>
-                    <div className='temp'>
-
-                    </div>
-
-                    <div className='stats'>
-                        <div className='left-stats'>
-                            <div className='item'>
-                                <span>Stat</span>
-                            </div>
-                            <div className='item'>
-                                <span>Stat</span>
-                            </div>
-                            <div className='item'>
-                                <span>Stat</span>
-                            </div>
-                        </div>
-                        <div className='right-stats'>
-                            <div className='item'>
-                                <span>Stat</span>
-                            </div>
-                            <div className='item'>
-                                <span>Stat</span>
-                            </div>
-                            <div className='item'>
-                                <span>Stat</span>
-                            </div>
-                        </div>
-                    </div>
                 </div>
-                <div className='right'>
-                    <img />
+                <div className='temp'>
+                    45^
+                </div>
+
+                <div className='stats'>
+                    <div className='left-stats'>
+                        <div className='item'>
+                            <span>Stat</span>
+                        </div>
+                        <div className='item'>
+                            <span>Stat</span>
+                        </div>
+                        <div className='item'>
+                            <span>Stat</span>
+                        </div>
+                    </div>
+                    <div className='right-stats'>
+                        <div className='item'>
+                            <span>Stat</span>
+                        </div>
+                        <div className='item'>
+                            <span>Stat</span>
+                        </div>
+                        <div className='item'>
+                            <span>Stat</span>``
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div className='right'>
+                <img className='map-image' src='https://static-maps.alltrails.com/production/marker_maps/lists/15507574/lists-15507574-v2-1631409590-600w350h-en-US-i-1-style_3.png' alt='Map'/>
+            </div>
         </div>
+    </div>
     )
 }
 
