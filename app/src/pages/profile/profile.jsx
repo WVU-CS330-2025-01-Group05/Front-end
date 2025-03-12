8a585c277d04a2803a235250b186590a114ae0c2
Lauren Taylor <laurenktaylor04@gmail.com>
2025-03-12 15:13:33 -0400

    profile is now react file

---
+++ app/src/pages/profile/profile.jsx
@@ -0,0 +1,35 @@
+import './profile.css'
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Profile</title>
+        <link rel="stylesheet" href="profile.css">
+        <script src="profile.js"></script>
+    </head>
+    <body>
+        <img src="test profile.jpg" alt="profile image">
+
+        <input id="back" type="button" value="Back to Home Page">
+        <input id="edit" type="button" value="Edit Profile">
+
+        <div>
+            <p id="name">{Name}</p>
+            <p id="username">{username}</p>
+            <p id="hikes">Hikes Completed: {num of hikes}</p>
+            <label for="bio">Bio:</label>
+            <p id="biography">{user's bio}</p>
+        </div>
+
+    </body>
+</html>
+
+function Profile(){
+return(
+
+
+
+)
+
+}
+export default Profile
