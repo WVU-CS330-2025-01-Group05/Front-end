4bf54d6ff62ece203525b2287f70e96acce915ba
Grace Hanson <hansongrace2004@gmail.com>
2025-03-12 15:21:31 -0400

    Update profile.jsx
    update to fit with JSX

--- app/src/pages/profile/profile.jsx
+++ app/src/pages/profile/profile.jsx
@@ -1,35 +1,29 @@
-import './profile.css'
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Profile</title>
-        <link rel="stylesheet" href="profile.css">
-        <script src="profile.js"></script>
-    </head>
-    <body>
-        <img src="test profile.jpg" alt="profile image">
+import React from 'react';
+import './profile.css';
 
-        <input id="back" type="button" value="Back to Home Page">
-        <input id="edit" type="button" value="Edit Profile">
+function Profile() {
+    //examples
+  const name = "Lauren Taylor"; 
+  const username = "LaurenKate25"; 
+  const numOfHikes = 25; 
+  const bio = "bio goes here"; 
 
-        <div>
-            <p id="name">{Name}</p>
-            <p id="username">{username}</p>
-            <p id="hikes">Hikes Completed: {num of hikes}</p>
-            <label for="bio">Bio:</label>
-            <p id="biography">{user's bio}</p>
-        </div>
+  return (
+    <div>
+      <img src="test profile.jpg" alt="profile image" />
 
-    </body>
-</html>
+      <input id="back" type="button" value="Back to Home Page" />
+      <input id="edit" type="button" value="Edit Profile" />
 
-function Profile(){
-return(
-
-
-
-)
-
+      <div>
+        <p id="name">{name}</p>
+        <p id="username">{username}</p>
+        <p id="hikes">Hikes Completed: {numOfHikes}</p>
+        <label htmlFor="bio">Bio:</label>
+        <p id="biography">{bio}</p>
+      </div>
+    </div>
+  );
 }
-export default Profile
+
+export default Profile;
