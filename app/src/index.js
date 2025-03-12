ad13c774d6db645d8ccfd61c198d2f98f3376d0c
Grace Hanson <hansongrace2004@gmail.com>
2025-03-12 15:26:34 -0400

    Update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import Home from "./pages/home/home"
 import Map from "./pages/map/map"
 import Friends from "./pages/friends/friends"
+import Profile from './pages/friends/friends'
 
 
 
@@ -18,6 +19,7 @@ root.render(
         <Route path="/" element={<Home />}/>
         <Route path='/map' element={<Map />}/>
         <Route path='/friends' element={<Friends />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </BrowserRouter>
 );
