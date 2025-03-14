f5f0cdaeaeb478b83b8137037a1297731360c660
Lauren Taylor <laurenktaylor04@gmail.com>
2025-03-13 22:21:22 -0400

    updated profile for react

--- app/src/pages/profile/profile.jsx
+++ app/src/pages/profile/profile.jsx
@@ -7,20 +7,26 @@ function Profile() {
   const username = "LaurenKate25"; 
   const numOfHikes = 25; 
   const bio = "bio goes here"; 
+  const hikeName = "Seneca Rocks";
+  const day = "2/14/2025";
 
   return (
     <div className='profile'>
-      <img src="test profile.jpg" alt="profile image" />
+      <img src={require ("./test profile.jpg")} alt="profile image" />
 
-      <a href='/'><button id='back'>Back to Home Page</button></a>
+      <a href='/map'><button id='back'>Back to Home Page</button></a>
       <a href='/'><button id='edit'>Edit Profile</button></a>
 
       <div>
         <p id="name">{name}</p>
-        <p id="username">{username}</p>
+        <p id="username">@{username}</p>
         <p id="hikes">Hikes Completed: {numOfHikes}</p>
         <label htmlFor="bio">Bio:</label>
         <p id="biography">{bio}</p>
+      </div>
+
+      <div className="hikes">
+        <p className="completedHike">{name} completed {hikeName} on {day}!</p>
       </div>
     </div>
   );
