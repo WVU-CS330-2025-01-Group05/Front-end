24026bde3d378f0139e8cf16e2dabbd19eed61bd
Lauren Taylor <laurenktaylor04@gmail.com>
2025-03-13 21:53:36 -0400

    converted profile completely to jsx

--- app/src/pages/profile/profile.jsx
+++ app/src/pages/profile/profile.jsx
@@ -7,20 +7,26 @@ function Profile() {
   const username = "LaurenKate25"; 
   const numOfHikes = 25; 
   const bio = "bio goes here"; 
+  const hikeName = "Seneca Rocks";
+  const date = "2/14/2025";
 
   return (
     <div>
-      <img src="test profile.jpg" alt="profile image" />
+      <img src={require ("./test profile.jpg")} alt="profile image" />
 
-      <input id="back" type="button" value="Back to Home Page" />
-      <input id="edit" type="button" value="Edit Profile" />
+      <input id="back" type="button" value="Back to Main Page"/>
+      <input id="edit" type="button" value="Edit Profile"/>
 
       <div>
         <p id="name">{name}</p>
-        <p id="username">{username}</p>
+        <p id="username">@{username}</p>
         <p id="hikes">Hikes Completed: {numOfHikes}</p>
         <label htmlFor="bio">Bio:</label>
         <p id="biography">{bio}</p>
+      </div>
+
+      <div className="completedHikes">
+        <p className="completedHike">{name} completed {hikeName} on {date}!</p>
       </div>
     </div>
   );
