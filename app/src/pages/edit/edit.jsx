24e8bf3b6d1f5c4036af3b5f46fc71fee8a56654
Lauren Taylor <laurenktaylor04@gmail.com>
2025-03-29 22:00:03 -0400

    updated edit profile

--- app/src/pages/edit/edit.jsx
+++ app/src/pages/edit/edit.jsx
@@ -8,11 +8,24 @@ function Edit() {
 
             <a href="/profile"><button id="back">Back to Account</button></a>
 
-            <div className="pfp">
-                <label for="pfp">Edit Profile Picture (upload SQUARE PNG, JPEG, or JPG)</label>
-                <input type="file" id="pfp" accept="image/png, image/jpeg, image/jpg" ></input>
-            </div>
+            <form>
+                <div className="pfp">
+                    <label for="pfp">Edit Profile Picture (upload SQUARE PNG, JPEG, or JPG)</label>
+                    <input type="file" id="file" accept="image/png, image/jpeg, image/jpg" ></input>
+                </div>
 
+                <div className="inputs">
+                    <label for="name">Enter Name: <br></br></label>
+                    <input type="text" id="name" size="53"></input><br></br><br></br>
+                    <label for="username">Enter New Username: <br></br></label>
+                    <input type="text" id="username" size="53"></input>
+                    <p>USERNAME IS ALREADY TAKEN</p>
+                    <label for="bio">Enter Bio: <br></br></label>
+                    <textarea id="bio" name="bio" rows="5" cols="50"></textarea>
+                </div>
+
+                <input type="submit" id="submit"></input>
+            </form>
         </div>
     );
 }
