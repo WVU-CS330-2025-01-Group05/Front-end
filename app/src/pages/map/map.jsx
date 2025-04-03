908a5b7c18e66875729f56ee2aaf32f044e907f4
Grace Hanson <hansongrace2004@gmail.com>
2025-04-02 22:36:47 -0400

    Add leaflet map

--- app/src/pages/map/map.jsx
+++ app/src/pages/map/map.jsx
@@ -1,90 +1,53 @@
-import Clock from './clock'
-import './map.css' 
-import { default as rain } from './icons/rain.svg';
-import { default as clouds } from './icons/clouds.svg';
-import { default as moon } from './icons/moon.svg';
-import { default as humidity } from './icons/humidity.svg';
-import { default as uv } from './icons/uv.svg';
-import { default as leaf } from './icons/leaf.svg';
+import Clock from './clock';
+import './map.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-
-
-function Map(){
-
-    
-
-
-
-    return(
-    <div className='map'>
-        <div className='header'>
-            <Clock/>
-            <span id="plan">Plan Your Hike</span>
-            <a href='/profile'><button id="account">Account</button></a>
-        </div>
-        <div className='bottom'>
-            <div className='left'>
-                <div className='filters'>
-                    <div className='filter'>
-                        <label for='filter1'>Select Something:</label>
-                        <select id='filter1'>
-                            <option>Location 1</option>
-                            <option>Location 2</option>
-                            <option>Location 3</option>
-                            <option>Location 4</option>
-                        </select>
+function Map() {
+    return (
+        <div className='map'>
+            <div className='header'>
+                <Clock />
+                <span id="plan">Plan Your Hike</span>
+                <a href='/profile'><button id="account">Account</button></a>
+            </div>
+            <div className='bottom'>
+                <div className='left'>
+                    <div className='filters'>
+                        <div className='filter'>
+                            <label htmlFor='filter1'>Select Something:</label>
+                            <select id='filter1'>
+                                <option>Location 1</option>
+                                <option>Location 2</option>
+                                <option>Location 3</option>
+                                <option>Location 4</option>
+                            </select>
+                        </div>
+                        <div className='filter'>
+                            <label htmlFor='filter2'>Select Trail:</label>
+                            <select id='filter2'>
+                                <option>Location 1</option>
+                                <option>Location 2</option>
+                                <option>Location 3</option>
+                                <option>Location 4</option>
+                            </select>
+                        </div>
                     </div>
-                    <div className='filter'>
-                        <label for='filter2'>Select Trail:</label>
-                        <select id='filter2'>
-                            <option>Location 1</option>
-                            <option>Location 2</option>
-                            <option>Location 3</option>
-                            <option>Location 4</option>
-                        </select>
+                    <div className='stats'>
+                        {/* Weather Stats Section */}
                     </div>
                 </div>
-                <div className='temp'>
+                <div className='right' style={{ height: "80vh", width: "70vw" }}>
+                <MapContainer center={[39.6295, -79.9559]} zoom={13} style={{ height: "100%", width: "100%" }}>
 
-                </div>
-
-                <div className='stats'>
-                    <div className='left-stats'>
-                        <div className='item'>
-                            <img src={rain} className='icon'/>
-                            <span>% Rain</span>
-                        </div>
-                        <div className='item'>
-                            <img src={moon} className='icon'/>
-                            <span>Moon</span>
-                        </div>
-                        <div className='item'>
-                            <img src={clouds} className='icon'/>
-                            <span>% Cloud</span>
-                        </div>
-                    </div>
-                    <div className='right-stats'>
-                        <div className='item'>
-                            <img src={humidity} className='icon'/>
-                            <span>Humidity: </span>
-                        </div>
-                        <div className='item'>
-                            <img src={uv} className='icon'/>
-                            <span>UV: </span>
-                        </div>
-                        <div className='item'>
-                            <img src={leaf} className='icon'/>
-                            <span>Leaf</span>
-                        </div>
-                    </div>
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    </MapContainer>
                 </div>
             </div>
-            <div className='right'>
-                <img src='https://static-maps.alltrails.com/production/marker_maps/lists/10447222/lists-10447222-v2-1518639504-600w350h-en-US-i-1-style_3.png'/>
-            </div>
         </div>
-    </div>
-    )
+    );
 }
 
-export default Map
+export default Map;
