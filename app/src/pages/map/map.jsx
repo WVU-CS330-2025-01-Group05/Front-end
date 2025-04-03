681cf5a836a1d4103310f8550cef0b741c4c51be
BlakeR4 <157330817+BlakeR4@users.noreply.github.com>
2025-04-03 01:55:47 -0400

    Update map.jsx

--- app/src/pages/map/map.jsx
+++ app/src/pages/map/map.jsx
@@ -1,6 +1,7 @@
+import React, {useState, useEffect} from 'react';
 import Clock from './clock';
 import './map.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { default as rain } from './icons/rain.svg';
 import { default as clouds } from './icons/clouds.svg';
@@ -8,9 +9,53 @@ import { default as moon } from './icons/moon.svg';
 import { default as humidity } from './icons/humidity.svg';
 import { default as uv } from './icons/uv.svg';
 import { default as leaf } from './icons/leaf.svg';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
+const customIcon = L.icon({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+    iconSize: [25, 41], 
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
+const defaultCenter = [39.6295, -79.9559];
+
+
+function LocationHandler({ setPosition }) {
+    
+const map = useMapEvents({
+    locationfound(e) {
+    setPosition(e.latlng);
+    map.flyTo(e.latlng, map.getZoom());
+      },
+        locationerror(e) {
+        alert(`Unable to determine location: ${e.message}`);
+      },
+      
+    // trigger on click (if needed)
+    // click() {
+    //   map.locate();
+    //   },
+    });
+  
+    
+    useEffect(() => {
+      map.locate({ setView: true, timeout: 20000, enableHighAccuracy: true });
+    }, [map]);
+  
+    return null;
+  }
 
 function Map() {
+    const [position, setPosition] = useState(null);
+  
+
     return (
         <div className='map'>
             <div className='header'>
@@ -22,12 +67,12 @@ function Map() {
                 <div className='left'>
                     <div className='filters'>
                         <div className='filter'>
-                            <label htmlFor='filter1'>Select Something:</label>
+                            <label htmlFor='filter1'>Filter:</label>
                             <select id='filter1'>
-                                <option>Location 1</option>
-                                <option>Location 2</option>
-                                <option>Location 3</option>
-                                <option>Location 4</option>
+                                <option>Lowest UV</option>
+                                <option>Lowest Pollen</option>
+                                <option>Closest</option>
+                                <option>Longest</option>
                             </select>
                         </div>
                         <div className='filter'>
@@ -44,15 +89,15 @@ function Map() {
                     <div className='left-stats'>
                         <div className='item'>
                             <img src={rain} className='icon'/>
-                            <span>% Rain</span>
+                            <span>% Rain:</span>
                         </div>
                         <div className='item'>
                             <img src={moon} className='icon'/>
-                            <span>Moon</span>
+                            <span>Moon:</span>
                         </div>
                         <div className='item'>
                             <img src={clouds} className='icon'/>
-                            <span>% Cloud</span>
+                            <span>% Cloud:</span>
                         </div>
                         <div className='right-stats'>
                         <div className='item'>
@@ -65,7 +110,7 @@ function Map() {
                         </div>
                         <div className='item'>
                             <img src={leaf} className='icon'/>
-                            <span>Leaf</span>
+                            <span>Pollen:</span>
                         </div>
                     </div>
 
@@ -73,11 +118,19 @@ function Map() {
                     </div>
                 </div>
                 <div className='right' style={{ height: "80vh", width: "70vw" }}>
-                <MapContainer center={[39.6295, -79.9559]} zoom={13} style={{ height: "100%", width: "100%" }}>
+                <MapContainer center={defaultCenter} zoom={13} style={{ height: "100%", width: "100%" }}>
 
                         <TileLayer
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
+                         <LocationHandler setPosition={setPosition} />
+                    
+                {position && (
+                    <Marker position={position} icon= {customIcon}>
+                        <Popup>You are here</Popup>
+                    </Marker>
+                )} 
+                
                     </MapContainer>
                 </div>
             </div>
