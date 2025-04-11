c24fb54ba54510fecc049764e2ce53659d31b951
JHays222 <108699497+JHays222@users.noreply.github.com>
2025-04-11 15:32:11 -0400

    F

--- app/src/pages/map/map.jsx
+++ app/src/pages/map/map.jsx
@@ -37,7 +37,7 @@ function LocationHandler({ setPosition }) {
             map.flyTo(e.latlng, map.getZoom());
         },
         locationerror(e) {
-            alert(`Unable to determine location: ${e.message}`);
+         
         },
 
         // trigger on click (if needed)
@@ -73,6 +73,7 @@ function Map() {
 
     //testing python 
     const [message, setMessage] = useState("");
+    const [distance, setDistance] = useState(0);
 
     const runPythonScript = async () => {
         const res = await fetch("http://localhost:5000/run-script", {
