c15dfa1eb5524086a63e5c05041bbf2400f44adb
Lauren Taylor <laurenktaylor04@gmail.com>
2025-05-05 12:25:02 -0400

    Update Register.js

--- app/src/pages/Register.js
+++ app/src/pages/Register.js
@@ -20,7 +20,7 @@ const Register = () => {
   const [bio] = useState('No Bio');
   const [img] = useState('/default pfp.jpg');
   const navigate = useNavigate();
-  const API_URL = process.env.REACT_APP_BACKEND_API_URL || 'cs330-2025-01-group05-backend.azurewebsites.net'; // Fallback for local development
+  const API_URL = process.env.REACT_APP_BACKEND_API_URL || 'https://cs330-2025-01-group05-backend.azurewebsites.net'; // Fallback for local development
   
   const [alertMessage, setAlertMessage] = useState('');
     const [showAlert, setShowAlert] = useState(false);
