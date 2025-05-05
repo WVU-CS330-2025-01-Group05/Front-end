83513d80f4c73697be11cbf2d84e2bed7a8f9a67
Lauren Taylor <laurenktaylor04@gmail.com>
2025-05-05 14:23:24 -0400

    fallback

--- app/src/pages/map/map.jsx
+++ app/src/pages/map/map.jsx
@@ -15,7 +15,7 @@ import { getTrailClimateData, getTrailClimateDataByCounty, generateSyntheticWeat
 import axios from 'axios';
 
 
-const API_URL = process.env.REACT_APP_BACKEND_API_URL;
+const API_URL = process.env.REACT_APP_BACKEND_API_URL || 'https://cs330-2025-01-group05-backend-fceefzc8c5gfemc7.eastus2-01.azurewebsites.net';
 
 
 // Cache for API responses
