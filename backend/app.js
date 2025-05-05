9b1b64174c5c35bf88439d5d2954f6924cce1aa0
Lauren Taylor <laurenktaylor04@gmail.com>
2025-05-05 12:45:30 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: 'https://gentle-river-0a6ef060f.6.azurestaticapps.net',
   credentials: true
 }));
 
