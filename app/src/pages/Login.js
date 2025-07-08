44e5e8fd03a9feb23a9f286d2497cc3357769cc0
Lauren Taylor <laurenktaylor04@gmail.com>
2025-07-07 21:00:33 -0400

    j

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -19,7 +19,7 @@ const Login = ({ setAuthenticated }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const API_URL = process.env.REACT_APP_BACKEND_API_URL || 'https://cs330-2025-01-group05-backend-fceefzc8c5gfemc7.eastus2-01.azurewebsites.net'; // Fallback for local development
+  const API_URL = process.env.REACT_APP_BACKEND_API_URL;// || 'https://cs330-2025-01-group05-backend-fceefzc8c5gfemc7.eastus2-01.azurewebsites.net'; // Fallback for local development
 
   const [alertMessage, setAlertMessage] = useState('');
   const [showAlert, setShowAlert] = useState(false);
